076445f41cfc32922ad3d240c44b0e3c0be97abc
Vlad Korobko <max.vlad.kor@gmail.com>
2022-12-26 14:58:52 +0200

    Practiced writing middleware

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-   res.send('Hi!')
+app.all('/hello', (req, res, next) => {
+    console.log('All');
+    next();
+});
+
+const callback = (req, res, next) => {
+    console.log('CB');
+    next();
+}
+
+app.get('/hello', callback, (req, res) => {
+   res.send('Hi!');
 });
 
 app.listen(port, () => {
