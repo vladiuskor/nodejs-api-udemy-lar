b2cf164f550c896b7bae8aa583ce343b4861e9f4
Vlad Korobko <max.vlad.kor@gmail.com>
2022-12-26 14:28:17 +0200

    Added simple server using native module http

---
+++ index.js
@@ -0,0 +1,23 @@
+import http from "http";
+
+const host = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+
+    switch (req.method) {
+        case 'GET':
+            switch (req.url) {
+                case '/hello':
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Hi!');
+                    break;
+            }
+            break;
+    }
+});
+
+server.listen(port, host, () => {
+    console.log(`Server is running on ${host}:${port}`);
+});
