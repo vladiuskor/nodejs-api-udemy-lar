91ae7610843e02715390b0a2075641e7bb3e1f65
Vlad Korobko <max.vlad.kor@gmail.com>
2022-12-26 20:54:58 +0200

    Made some practice about middleware

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import {userRouter} from "./users/users.js";
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`Time is ${Date.now()}`);
+    next();
+});
+
 app.get('/hello', (req, res) => {
    res.status(200).send({
        success: true
@@ -11,6 +16,11 @@ app.get('/hello', (req, res) => {
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).send(err.message);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- users/users.js
+++ users/users.js
@@ -1,6 +1,10 @@
 import express from "express";
-
 const userRouter = express.Router();
+
+userRouter.use((req, res, next) => {
+    console.log('User processor!');
+    next();
+})
 
 userRouter.post('/login', (req, res) => {
     res.send('Login');
